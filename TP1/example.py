b4599b49104b1940f3df2a74abeb689a9b05549d
Natalia Mellino <natimellino.nb@gmail.com>
2022-04-08 21:54:21 -0300

    Progreso

--- TP1/example.py
+++ TP1/example.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_iris
 from sklearn import tree
+
 iris = load_iris()
 X = iris.data
 y = iris.target
